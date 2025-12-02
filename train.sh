a44eb219df1e12750326a83efd6af6a4d1684544
revistain <robin150@naver.com>
2025-12-02 03:56:47 +0000

    .

--- train.sh
+++ train.sh
@@ -11,20 +11,11 @@ OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=0 python3 train_wandb.py \
     --use_thermal_adapter \
     --use_alignment_loss
 
-#    --use_sepearte_backbone_lr
-#    --backbone_lr 1e-5
-#    --lr 1e-5
-#    --use_alignment_loss
-#    --use_GeMAdditionalLayer
-#    --comment "baseline_clip-cls-loss_al0.5_GeMLinear[784,ReLU,784]_seperateLR[1e-5,1e-4]" \
-#    --comment "baseline_alignment-attn[dot_product,rgb-attn-map]-loss_al1.0_num-train-block6" \
-#    --negs_num_per_query 10
-#    --margin 0.1
 
 ######### 설명 #########
 # comment: wandb 기록명
 # use_alignment_loss: alignment loss 사용 여부
 # use_sepearte_backbone_lr/backbone_lr: backbone과 나머지 모듈의 learning rate를 다르게 설정
 # use_GeMAdditionalLayer: GeM pooling 후에 Linear-ReLU-Linear 추가
-    # --use_rgb_adapter
-    # --use_thermal_adapter
+# use_rgb_adapter     : adapter 사용여부(없으면 미사용)
+# use_thermal_adapter : adapter 사용여부(없으면 미사용)
