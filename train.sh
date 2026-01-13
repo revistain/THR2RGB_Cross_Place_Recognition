e6707bb8e199caf2498c3429a3a82e46e47a7b2b
revistain <kimrobin150@gmail.com>
2026-01-13 12:15:21 +0000

    back to thermal LoFTR

--- train.sh
+++ train.sh
@@ -9,17 +9,17 @@ python3 train_wandb.py \
     --queries_per_epoch 2000 \
     --num_trainable_blocks_RGB 4 \
     --num_trainable_blocks_THERMAL 4 \
-    --comment "test-croco-0.8-alpha10.0-decDepth8-rerank" \
+    --comment "test-croco-0.8-alpha10.0-decDepth8-rerank-contrastLoss" \
     --margin 0.1 \
     --epochs_num 100 \
     --negs_num_per_query 10 \
     --croco_mask_ratio 0.8 \
     --num_decoder_depth 8 \
     --recon_weight 10 \
+    --use_contrastive_recon_loss \
     --recon_loss_type 'mse' \
     --use_reranking
 
-    # --use_contrastive_recon_loss \
     # -ssim-contrastLoss
     # --use_confidence_map
     # --use_ssim_recon_loss
