a88722a25db8fb5c1821590ca7e1a842afec6376
revistain <kimrobin150@gmail.com>
2026-01-14 03:06:41 +0000

    back to thermal LoFTR

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 NCCL_P2P_DISABLE=1 \
 OMP_NUM_THREADS=8 \
-CUDA_VISIBLE_DEVICES=4 \
+CUDA_VISIBLE_DEVICES=2 \
 python3 train_wandb.py \
     --save_dir './logs' \
     --features_dim 768 \
@@ -9,17 +9,16 @@ python3 train_wandb.py \
     --queries_per_epoch 2000 \
     --num_trainable_blocks_RGB 4 \
     --num_trainable_blocks_THERMAL 4 \
-    --comment "test-croco-0.8-alpha10.0-decDepth8-rerank-contrastLoss" \
+    --comment "test-croco-0.8-alpha10.0-decDepth8-mse+ssim-reranking" \
     --margin 0.1 \
     --epochs_num 100 \
     --negs_num_per_query 10 \
     --croco_mask_ratio 0.8 \
     --num_decoder_depth 8 \
     --recon_weight 10 \
-    --use_contrastive_recon_loss \
-    --recon_loss_type 'mse' \
+    --recon_loss_type 'mse+ssim' \
     --use_reranking
-
+    # --use_contrastive_recon_loss \
     # -ssim-contrastLoss
     # --use_confidence_map
     # --use_ssim_recon_loss
