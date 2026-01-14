8dfa6b67471d0d292a63d29df217c01186abcbdc
revistain <kimrobin150@gmail.com>
2026-01-14 07:27:34 +0000

    remove useless reranking loss

--- train_wandb.py
+++ train_wandb.py
@@ -27,104 +27,6 @@ from croco.models.criterion import MaskedMSE
 from recon_vis import visualize_during_training
 from info_nce import InfoNCE, info_nce
 
-def clip_patch_alignment_mean_loss(thermal_patches, rgb_patches, temperature=0.07):
-    # https://taeyuplab.tistory.com/16
-    B, N, D = thermal_patches.shape # (batch, 16x16, feature_dim)
-    
-    # 
-    thermal_feat = thermal_patches.mean(dim=1)
-    rgb_feat = rgb_patches.mean(dim=1)
-    
-    thermal_feat = F.normalize(thermal_feat, dim=-1)
-    rgb_feat = F.normalize(rgb_feat, dim=-1)
-    
-    logits = torch.matmul(thermal_feat, rgb_feat.T) / temperature
-    labels = torch.arange(B, device=logits.device)
-    
-    loss_t2r = F.cross_entropy(logits, labels)
-    # loss_r2t = F.cross_entropy(logits.T, labels)
-    
-    # FIXME: loss_r2t는 필요없지 않나
-    # return (loss_t2r + loss_r2t) / 2
-    return loss_t2r
-
-def clip_alignment_cls_loss(thermal_cls, rgb_cls, temperature=0.07):
-    """
-    thermal_cls: (B, D) - CLS token features
-    rgb_cls: (B, D) - CLS token features
-    """
-    thermal_feat = F.normalize(thermal_cls, dim=-1)
-    rgb_feat = F.normalize(rgb_cls, dim=-1)
-    
-    logits = torch.matmul(thermal_feat, rgb_feat.T) / temperature
-    labels = torch.arange(thermal_feat.size(0), device=logits.device)
-    
-    loss_t2r = F.cross_entropy(logits, labels)
-    
-    # FIXME: 이거 loss_r2t도 넣어서 해보기, 필요한거 같음
-    return loss_t2r
-
-def patch_alignment_loss(thermal_patches, rgb_patches):
-    """
-    thermal_patches: (B, 256, 768)
-    rgb_patches: (B, 256, 768)
-    """
-    # L2 normalize
-    thermal_norm = F.normalize(thermal_patches, dim=-1)  # (B, 256, 768)
-    rgb_norm = F.normalize(rgb_patches, dim=-1)          # (B, 256, 768)
-    
-    # 같은 위치 patch끼리 cosine similarity
-    cos_sim = (thermal_norm * rgb_norm).sum(dim=-1)  # (B, 256)
-    
-    # similarity가 1에 가까울수록 좋음
-    loss = 1 - cos_sim.mean()
-    return loss
-
-def attention_weighted_patch_alignment_loss(thermal_patches, rgb_patches, thermal_attn, rgb_attn, use_intranorm=False):
-    """
-    공통 중요도로 가중치를 준 patch alignment
-    
-    Args:
-        thermal_patches: (B, num_patches, 768)
-        rgb_patches: (B, num_patches, 768)
-        thermal_attn: (B, num_heads, num_tokens)
-        rgb_attn: (B, num_heads, num_tokens)
-        use_intranorm: if True, L2-normalize patches before averaging (GeM-style)
-    
-    Returns:
-        loss
-    """
-    # Multi-head averaging + CLS 토큰 제거
-    thermal_attn_flat = thermal_attn.mean(dim=1)[:, 1:]  # (B, num_patches)
-    rgb_attn_flat = rgb_attn.mean(dim=1)[:, 1:]  # (B, num_patches)
-    
-    # 공통 중요도
-    # importance = thermal_attn_flat * rgb_attn_flat
-    # importance = importance / (importance.sum(dim=1, keepdim=True) + 1e-8)
-    importance = rgb_attn_flat
-    importance = importance / (importance.sum(dim=1, keepdim=True) + 1e-8)
-    
-    if use_intranorm:
-        thermal_patches = F.normalize(thermal_patches, dim=-1)
-        rgb_patches = F.normalize(rgb_patches, dim=-1)
-        
-        thermal_agg = (thermal_patches * importance.unsqueeze(-1)).sum(dim=1)
-        rgb_agg = (rgb_patches * importance.unsqueeze(-1)).sum(dim=1)
-        
-        # F.cosine_similarity 사용
-        similarity = F.cosine_similarity(thermal_agg, rgb_agg, dim=-1)
-        loss = 1 - similarity.mean()
-    else:
-        ...
-        # # 원래 방식: Patch-wise cosine similarity
-        # patch_sim = (thermal_patches * rgb_patches).sum(dim=-1)  # (B, num_patches)
-        
-        # # Importance-weighted similarity
-        # weighted_sim = (patch_sim * importance).sum(dim=1)  # (B,)
-        # loss = 1 - weighted_sim.mean()
-    
-    return loss
-
 def set_seed(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -352,70 +254,10 @@ if __name__ == "__main__":
                     # triplet_loss
                     overall_loss += triplet_loss
                 
-                rerank_loss = 0
-                if args.use_rerank_loss:
-                    # TODO: augmentation 문제 없는지도 확인
-                    # constrastive_reconstruction loss 구하기
-                    query_lists = [i for i in range(0, args.train_batch_size)] 
-                    positive_lists = [i for i in range(1, args.train_batch_size * (args.negs_num_per_query+2), (args.negs_num_per_query+2))] 
-                    negative_lists = [i 
-                        for batch_idx in range(args.train_batch_size)
-                        for i in range(
-                            batch_idx * (args.negs_num_per_query + 2) + 2,  # start+2부터
-                            (batch_idx + 1) * (args.negs_num_per_query + 2)  # start+12까지
-                        )
-                    ]
-                    
-                    query_embedding = masked_patch_embedding[query_lists].detach()
-                    positive_embedding = patch_embedding[positive_lists].detach()
-                    negative_embedding = patch_embedding[negative_lists].detach()
-                    
-                    # 아침에 여기 고치기
-                    # loss는 어떻게? 비율은?
-                    
-                    # 2. 전부 pos embedding 추가
-                    decoder_pos_embed = model.module.decoder_pos_embed
-                    query_embedding = query_embedding + decoder_pos_embed
-                    positive_embedding = positive_embedding + decoder_pos_embed
-                    negative_embedding = negative_embedding + decoder_pos_embed
-
-                    decoded_embeddings = torch.zeros(
-                        (args.train_batch_size * (args.negs_num_per_query+1), 256, args.features_dim), device=args.device)
-                    
-                    # c. query를 batch로 만들어주기
-                    for batch_idx in range(args.train_batch_size):
-                        query_embedding_batch = query_embedding[batch_idx,:,:].unsqueeze(0).expand((1+args.negs_num_per_query), -1, -1)  # [RERANKING_TOP_K, N_visible, 768]
-                        
-                        # d. pos/neg 순으로 쌓아주기 (pos, pos, pos, pos, neg, neg ... neg)
-                        pos_neg_embedding_batch = torch.cat(
-                            [positive_embedding[batch_idx,:,:].unsqueeze(0),
-                             negative_embedding[batch_idx*args.negs_num_per_query:(batch_idx+1)*args.negs_num_per_query,:,:]], dim=0)
-                        
-                        # 3. decoder 통과시키기
-                        # 3-1. Thermal decoder 통과
-                        thermal_dec = query_embedding_batch
-                        for blk in model.module.decoder_blocks:
-                            thermal_dec = blk(thermal_dec, pos_neg_embedding_batch)
-                        thermal_dec = model.module.decoder_norm(thermal_dec)
-                        
-                        decoded_embeddings[batch_idx*(args.negs_num_per_query+1):(batch_idx+1)*(args.negs_num_per_query+1)] = thermal_dec
-                    
-                    # 5. infoNCE loss 계산
-                    infoNCE()
-                    decoded_embeddings.shape # (B*12,256,768)
-
-                    # 5. descriptor 기반의 infoNCE loss
-                    
-                    # GAP하고 infoNCE loss 계산(의미 있나?)
-                    # 그리고 GAP를 한다는 것은 global한 레벨에서 본다는것
-                    # 그럼 GeM pooling하는 방법과 다른게 무엇인가?
-                    
-
                 # train_batch_size: 4, arg.negs_num_per_query: 10
                 recon_weight = args.recon_weight
                 rerank_weight = args.rerank_weight
                 overall_loss += (recon_loss * recon_weight)
-                overall_loss += (rerank_loss * rerank_weight)
                 overall_loss /= (args.train_batch_size * args.negs_num_per_query)
 
                 del global_features, query_features, positive_features, negative_features
@@ -432,7 +274,6 @@ if __name__ == "__main__":
                     "train/overall_loss": overall_loss,
                     "train/triplet_loss(scaled)": triplet_loss_sum.item() / (args.train_batch_size * args.negs_num_per_query),
                     "train/recon_loss(scaled)": (recon_loss * recon_weight).item() / (args.train_batch_size * args.negs_num_per_query) if isinstance(recon_loss, torch.Tensor) else 0,
-                    "train/rerank_loss(scaled)": (rerank_loss * rerank_weight).item() / (args.train_batch_size * args.negs_num_per_query) if isinstance(rerank_loss, torch.Tensor) else 0,
                 }, step=global_step)
                 
                 global_step += 1
@@ -507,31 +348,3 @@ if __name__ == "__main__":
         logging.info(f"================================================")
 
     wandb.finish()
-    
-    
-                    # 3. decoder 통과시키기
-                    # 3-1. Thermal decoder 통과
-                    # negs_num = args.negs_num_per_query
-                    # decoded_embeddings = torch.zeros(
-                    #     (args.train_batch_size * (negs_num+1), 256, args.features_dim), device=args.device)   
-                    
-                    # for query_idx in range(query_embedding.size(0)):
-                    #     # positive-query decoder 통과
-                    #     thermal_dec = query_embedding[query_idx,:,:]
-                    #     positive_dec = positive_embedding[query_idx]
-                    #     print(f"query: {query_idx} / positive: {query_idx*(negs_num+1)}")
-                        
-                    #     for blk in model.module.decoder_blocks:
-                    #         decoded_embeddings[query_idx*(negs_num+1)] = blk(thermal_dec, positive_dec)
-                    #     decoded_embeddings[query_idx*(negs_num+1)] = model.module.decoder_norm(decoded_embeddings[query_idx*negs_num])                        
-                        
-                    #     # negatives-query decoder 통과
-                    #     for negative_idx in range(negs_num):
-                    #         negative_dec = negative_embedding[query_idx*(negs_num+1)+negative_idx]
-                        
-                    #         print(f"query: {query_idx} / negative: {query_idx*(negs_num+1)+negative_idx}")
-                    #         for blk in model.module.decoder_blocks:
-                    #             decoded_embeddings[query_idx*(negs_num+1)+negative_idx+1] = blk(thermal_dec, negative_dec)
-                    #         decoded_embeddings[query_idx*(negs_num+1)+negative_idx+1] = model.module.decoder_norm(decoded_embeddings[query_idx*(negs_num+1)+negative_idx+1]) 
-
-                    
