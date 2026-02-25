fc33890533bee64cbe4d62f5566a227c9e3e5f76
revistain <kimrobin150@gmail.com>
2026-02-25 07:57:28 +0000

    .

--- train_CroCo.sh
+++ train_CroCo.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=3
 
 NCCL_P2P_DISABLE=1 \
 OMP_NUM_THREADS=4 \
@@ -10,14 +10,17 @@ python3 train_wandb.py \
     --lr 3e-5 \
     --train_seq Campus \
     --test_seq Urban Residential \
-    --comment "CroCo-beforeA6000(Sthereo,lr3e-5)-interintraRecon(r0.8, fixed, CLSmask)-distanceCLS" \
-    --croco_mask_ratio 0.8 \
+    --comment "CroCo-beforeA6000(Sthereo,lr3e-5)-interintraRecon(r0.9, fixed, CLSmask)" \
+    --croco_mask_ratio 0.9 \
     --recon_weight 10 \
     --use_recon_loss \
     --recon_loss_type 'l1' \
     --num_decoder_depth 8 \
-    --masking_method CLS \
-    --use_dist_cls_while_Recon
+    --masking_method CLS
+
+
+    # --use_dist_cls_while_Recon \
+    # --distance_weight 1.0
 
 # Scene 종류 : ['Campus', 'Residential', 'Urban', 'KAIST', 'SNU', 'Valley'] 
 # recon_loss_type: ['mse', 'l1', 'ssim', 'mse+ssim']
