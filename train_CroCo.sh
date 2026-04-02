cbac276a6f4ca180640abc6255336d22570f42cc
revistain <kimrobin150@gmail.com>
2026-04-02 04:54:40 +0000

    ,

--- train_CroCo.sh
+++ train_CroCo.sh
@@ -1,20 +1,21 @@
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=0
 
 NCCL_P2P_DISABLE=1 \
 OMP_NUM_THREADS=4 \
 CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
 python3 train_wandb.py \
     --cuda_device $CUDA_VISIBLE_DEVICES \
-    --foundation_model_path /home/jwkim/workspace/THR2RGB_Cross_Place_Recognition/backbone/dinov2/pretrained/dinov2_vits14_pretrain.pth \
+    --foundation_model_path /home/jwkim/workspace/THR2RGB_Cross_Place_Recognition/backbone/dinov2/pretrained/dinov2_vitb14_pretrain.pth \
     --save_dir './logs' \
     --lr 3e-5 \
-    --train_seq KAIST \
-    --test_seq SNU Valley \
-    --comment "CroCo-beforeA6000(Sthereo,lr3e-5)-posAsPaired" \
+    --train_batch_size 4 \
+    --train_seq Campus \
+    --test_seq Urban Residential \
+    --comment "CroCo-beforeA6000(Sthereo,lr3e-5)-posAsPaired_DINOv2-b" \
     --croco_mask_ratio 0.8 \
     --recon_weight 10 \
     --use_recon_loss \
-    --recon_loss_type 'l1' \
+    --recon_loss_type 'mse+ssim' \
     --num_decoder_depth 8 \
     --use_pos_as_aligned_rgb
 
--- train_GeM.sh
+++ train_GeM.sh
@@ -1,16 +1,17 @@
-CUDA_VISIBLE_DEVICES=0
+CUDA_VISIBLE_DEVICES=1
 
 NCCL_P2P_DISABLE=1 \
 OMP_NUM_THREADS=4 \
 CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
 python3 train_wandb.py \
     --cuda_device $CUDA_VISIBLE_DEVICES \
-    --foundation_model_path /home/jwkim/workspace/THR2RGB_Cross_Place_Recognition/backbone/dinov2/pretrained/dinov2_vits14_pretrain.pth \
+    --foundation_model_path /home/jwkim/workspace/THR2RGB_Cross_Place_Recognition/backbone/dinov2/pretrained/dinov2_vitb14_pretrain.pth \
     --save_dir './logs' \
+    --train_batch_size 4 \
     --lr 3e-5 \
-    --train_seq KAIST \
-    --test_seq SNU Valley \
-    --comment "GeM-beforeA6000(Sthereo,lr3e-5)"
+    --train_seq Campus \
+    --test_seq Urban Residential \
+    --comment "GeM-beforeA6000(Sthereo,lr3e-5)_DINOv2-b"
 
 # Scene 종류 : ['Campus', 'Residential', 'Urban', 'KAIST', 'SNU', 'Valley'] 
 # recon_loss_type: ['mse', 'l1', 'ssim', 'mse+ssim']
